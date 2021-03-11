c7d263d24cd4155e309a4e7de279a8bb87c6f3d1
Peter Zettl <mpzettl@gmail.com>
2021-03-11 16:43:41 +0000

    moved newPointStructure down in the program

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -11,34 +11,6 @@ const oldPointStructure = {
   8: ['J', 'X'],
   10: ['Q', 'Z']
 };
-const newPointScorer = {
-  a: 1,
-  b: 3,
-  c: 3,
-  d: 2,
-  e: 1,
-  f: 4,
-  g: 2,
-  h: 4,
-  i: 1,
-  j: 8,
-  k: 5,
-  l: 1,
-  m: 3,
-  n: 1,
-  o: 1,
-  p: 3,
-  q: 10,
-  r: 1,
-  s: 1,
-  t: 1,
-  u: 1,
-  v: 4,
-  w: 4,
-  x: 8,
-  y: 4,
-  z: 10
-}
 
 let simpleScorer = function(word){
 	let letterPoints = "";
@@ -122,7 +94,34 @@ function scorerPrompt(word) {
 
 function transform() {};
 
-let newPointStructure;
+let newPointStructure = {
+  a: 1,
+  b: 3,
+  c: 3,
+  d: 2,
+  e: 1,
+  f: 4,
+  g: 2,
+  h: 4,
+  i: 1,
+  j: 8,
+  k: 5,
+  l: 1,
+  m: 3,
+  n: 1,
+  o: 1,
+  p: 3,
+  q: 10,
+  r: 1,
+  s: 1,
+  t: 1,
+  u: 1,
+  v: 4,
+  w: 4,
+  x: 8,
+  y: 4,
+  z: 10
+}
 
 function runProgram() {
    initialPrompt();
